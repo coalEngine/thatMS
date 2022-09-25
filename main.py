48926bda318ce74ea4382b43a8b00bc9ce6b4bbd
Thiery Jean Baptiste <Thiery@ELIEs-MacBook-Pro.local>
2022-09-25 08:32:02 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+# Initialization
+pygame.init()
+
+# Window Handling
+window = pygame.display.set_mode((800, 600))
+
+
+
+running = True
+while running:
+    window.fill((0, 0, 0))
+    pygame.display.update()
+    for evt in pygame.event.get():
+        if evt.type == pygame.QUIT:
+            running = False
+
+
