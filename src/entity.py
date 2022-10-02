cb929a28e18efb80e6e9d1d0e7763dcafaca0d5f
Thiery <weirdogaming@gmail.com>
2022-10-02 17:33:15 -0400

    Update

---
+++ src/entity.py
@@ -0,0 +1,13 @@
+class Entity():
+    def __init__(self, name, hp, mana, x, y ):
+        self.name = name
+        self.hp = hp
+        self.mana = mana
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import pygame
 # Initialization
 pygame.init()
 
+
 # Window Handling
 window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("That Magic Swordsmen")
