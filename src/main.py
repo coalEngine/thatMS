f909b8265aea68fd39257d63c24345e632f4c078
Thiery <weirdogaming@gmail.com>
2022-10-09 20:51:26 -0400

    Update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame
 import sprites
+from entity import Entity
 
 
 # Initialization
@@ -7,7 +8,7 @@ pygame.init()
 
 
 # Window Handling
-pygame.display.set_caption("That Magical Swordsmen")
+pygame.display.set_caption("Traveling Magical Swordsmen")
 window = pygame.display.set_mode((800, 600))
 BG = (50, 50, 50)
 BLACK = (0,0,0)
@@ -15,18 +16,44 @@ BLACK = (0,0,0)
 # Sprites
 player_img = pygame.image.load("res/tmdS.png").convert_alpha()
 Sprite = sprites.Spritesheet(player_img)
+# Animation
+animation_list = []
+player_animation_steps = [2, 2]
+action = 0
+last_Time = pygame.time.get_ticks()
+animation_runtime = 1000
+frame = 0
+step_counter = 0
 
 
-player_frame_0 = Sprite.get_sprite(0, 32, 32, 3)
-player_frame_2 = Sprite.get_sprite(2, 32, 32, 3)
+for animation in player_animation_steps:
+    temp_img_list = []
+    for _ in range(animation):
+        temp_img_list.append(Sprite.get_sprite(step_counter, 32, 32, 3))
+        step_counter += 1
+    animation_list.append(temp_img_list)
+
 run = True
 while run:
     window.fill(BG)
-    window.blit(player_frame_0, (0,0))
-    window.blit(player_frame_2, (140, 0))
+    # Update Animation
+    current_time = pygame.time.get_ticks()
+
+    if current_time - last_Time >= animation_runtime:
+        frame += 1
+        last_Time = current_time
+        if frame >= len(animation_list[action]):
+            frame = 0
+
+    window.blit(animation_list[action][frame], (0, 0))
     for evt in pygame.event.get():
         if evt.type == pygame.QUIT:
             run = False
+        if evt.type == pygame.KEYDOWN:
+            if evt.key == pygame.K_d:
+                action = 0
+            if evt.key == pygame.K_a:
+                action = 1
 
     pygame.display.update()
 
